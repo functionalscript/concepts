30c58c35a47d1cbd53276a02273601626b477b4d
Trinidadec <msden.ua@gmail.com>
2021-11-20 22:29:25 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const i = require('.')
 
 let root = undefined
-root = i.insert(root)('hello world!')('second value')
+root = i.insert(root)('hello world!')
 root = i.insert(root)('a')
 
 console.log(root)
