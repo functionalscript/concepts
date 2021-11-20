0dba96a62626c3a2c1fb748e203de08173c0ece1
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2021-11-20 11:35:58 -0800

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const lib = require('functionalscript/lib')
  */
 
 /** @type {(node: TNode) => (newValue: string) => TNode} */
-const insert = ({left, value, right }) => newvalue => {
+const insert = ({left, value, right }) => newValue => {
   if (value === newValue) { return node }
   return lib.todo()
 }
