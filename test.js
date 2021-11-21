c227fb8f1f2807a86e34c22f62007afac40a6395
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2021-11-21 12:24:51 -0800

    Update test.js

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ for (let k = 0; k < 100; k++) {
   root = i.insert(root)((k * k).toString())
 }
 
-console.log(JSON.stringify(root, undefined, 2))
+// console.log(JSON.stringify(root, undefined, 2))
 
 console.log(`add3: ${add3(2)(2)}`)
 console.log(`add4: ${add4(2)(2)}`)
