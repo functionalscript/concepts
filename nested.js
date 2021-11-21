b83e460ed9b85d05ad249bf09b2210960c7c9f09
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2021-11-21 12:37:22 -0800

    Update nested.js

--- nested.js
+++ nested.js
@@ -1,7 +1,18 @@
+// Language without nested functions
 
 function static_func(a, b) {
   // no nested functions allowed
   return a + b
 }
 
-//
+static_func(2, 3) // 5
+
+// Language with nested functions
+
+function add(a) {
+  function f(b) {
+    return a + b
+  }
+}
+
+add(2)(3) // 5
