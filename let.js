91ec1d1e89f68e2a7736f56fc7a6b3ce1d1a1133
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2021-11-21 12:16:20 -0800

    Update let.js

--- let.js
+++ let.js
@@ -1,16 +1,20 @@
+/** @type {(a: number) => (b: number) => number}
 const add = a => b => a + b
 
+/** @type {(a: number) => (b: number) => number}
 const add2 = a => {
   const x = a
   return b => x + b
 }
 
+/** @type {(a: number) => (b: number) => number}
 const add3 = a => {
   let x = a
   x = 5
   return b => x + b // ERROR!
 }
 
+/** @type {(a: number) => (b: number) => number}
 const add4 = a => {
   let x = a
   const y = x
